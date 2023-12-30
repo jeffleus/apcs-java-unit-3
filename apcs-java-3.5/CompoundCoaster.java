3d80ca7e7c6d915ed53222ea823c2c20e185bec6
ucla-jeffl <jleininger@athletics.ucla.edu>
2023-12-30 13:34:36 -0800

    Create CompoundCoaster.java
    setup a project file for the Compound Roller Coaster

---
+++ apcs-java-3.5/CompoundCoaster.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class CompoundCoaster 
+{
+    public static void main(String[] args)
+    {        
+        // Program Header for Clarity in Terminal
+        System.out.println("*".repeat(40));
+        System.out.println("* EXERCISE 3.5.7 - COMPOUND COASTER");
+        System.out.println("*".repeat(40));
+
+
+        Scanner input = new Scanner(System.in);        
+    }
+}
+/*
+    Modify your previous exercise to determine if the user can ride a rollercoaster.
+
+    To ride the rollercoaster, you must be at least 42 inches tall. You must also be at least 9 years old.
+
+    Ask the user how tall and how old they are. Use ONE if-else to determine if the user can ride the rollercoaster. Note how much cleaner this is and how much less repetition this method has.
+
+    If they can, print “Welcome aboard!”
+    If they cannot, print “Sorry, you are not eligible to ride”
+*/
