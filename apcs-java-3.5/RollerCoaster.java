a0f1c093c860c689a00c136dd5c320b99ec69962
ucla-jeffl <jleininger@athletics.ucla.edu>
2023-12-30 13:34:25 -0800

    Create RollerCoaster.java
    setup a project file for the RollerCoaster

---
+++ apcs-java-3.5/RollerCoaster.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class RollerCoaster 
+{
+    public static void main(String[] args)
+    {
+        // Program Header for Clarity in Terminal
+        System.out.println("*".repeat(40));
+        System.out.println("* EXERCISE 3.5.6 - ROLLER COASTER");
+        System.out.println("*".repeat(40));
+
+        Scanner input = new Scanner(System.in); 
+    }
+}
+/*
+    Write a program that determines if the user can ride a rollercoaster.
+
+    To ride the rollercoaster, you must be at least 42 inches tall.
+    You must also be at least 9 years old.
+
+    Ask the user how tall and how old they are.
+    Use a nested if statement to determine if they can ride the rollercoaster.
+
+    If they can, print “Welcome aboard!”
+    If they cannot, print “Sorry, you are not eligible to ride”
+*/
