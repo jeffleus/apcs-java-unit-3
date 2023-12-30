ea182b0d24486eb2c4ed68b9fa1ee0316e2231b4
ucla-jeffl <jleininger@athletics.ucla.edu>
2023-12-30 13:35:32 -0800

    Create FindMedian.java
    setup a project file for the final exercise in the unit, FindTheMedian

---
+++ apcs-java-3.5/FindMedian.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class FindMedian 
+{
+    public static void main(String[] args)
+    {
+        // Program Header for Clarity in Terminal
+        System.out.println("*".repeat(40));
+        System.out.println("* EXERCISE 3.5.9 - FIND THE MEDIAN");
+        System.out.println("*".repeat(40));
+
+        // Ask the user for three ints and 
+        // print out the median.
+    }
+}
+
+/*
+    In this program you will ask the user for three integers and
+    then print out the median (middle) value.
+
+    Remember that a number is the median value if it is larger than one number and smaller than the other. For example, given the numbers 1, 3, 2 or the numbers 2, 3, 1, the middle number is 2.
+
+    Use compound boolean expressions to help you.
+
+    Example Output:
+
+    Enter the first integer: 
+    89
+    Enter the second integer: 
+    1000
+    Enter the third integer: 
+    23
+
+    The median is 89
+ */
